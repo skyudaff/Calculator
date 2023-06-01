f8f96daf213eb727b6dd4c7a71e99e2fca16fe95
Vladimir Razuvaev <skyudaff@mail.ru>
2023-06-01 14:24:31 +0300

    removed .idea modified Calc

--- src/Calculator.java
+++ src/Calculator.java
@@ -7,8 +7,15 @@ public class Calculator {
     BinaryOperator<Integer> minus = (x, y) -> x - y;
     BinaryOperator<Integer> multiply = (x, y) -> x * y;
 
-    //Реализация проверки деления на ноль через тернарный оператор
-    BinaryOperator<Integer> devide = (x, y) -> y != 0 ? x / y : 0;
+    //Добавлен вывод сообщения о невозможности делить на ноль,
+    // в качестве результата возращаем Integer.MIN_VALUE
+    BinaryOperator<Integer> devide = (x, y) -> {
+        if (y == 0) {
+            System.out.println("Ошибка! Делить на ноль нельзя");
+            return Integer.MIN_VALUE;
+        }
+        return x / y;
+    };
 
     UnaryOperator<Integer> pow = x -> x * x;
     UnaryOperator<Integer> abs = x -> x > 0 ? x : x * -1;
